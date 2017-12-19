f148a4128d479414f67398352f5cc63203df5364
ZacharyJohnson <zaj6827@gmail.com>
2017-12-19 11:23:46 -0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum;
+            string operat;
+            int secondNum;
+            int answer;
+
+            Console.WriteLine("Welcome to the Calculator. Please enter a number");
+            Console.Write("Enter your first number");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your operation (*, /, +, or -)");
+            operat = Console.ReadLine();
+            Console.Write("Enter your second number");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+            switch (operat)
+            {
+                    case "*":
+                        answer = firstNum * secondNum;
+                    Console.WriteLine(answer);
+                    break;
+                    case "/":
+                        answer = firstNum / secondNum;
+                    Console.WriteLine(answer);
+                    break;
+                    case "+":
+                        answer = firstNum + secondNum;
+                    Console.WriteLine(answer);
+                    break;
+                    case "-":
+                        answer = firstNum - secondNum;
+                    Console.WriteLine(answer);
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
